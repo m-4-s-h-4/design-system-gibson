b38d435d03b94f5f534ec9d9ab5542f71d75e961
Maria <maria123321borovikova@gmail.com>
2024-06-19 17:53:04 +0200

    final

--- build.cjs
+++ build.cjs
@@ -6,10 +6,21 @@ const jsonFilePath = path.join(__dirname, "tokens", "tokens.json");
 const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, "utf8"));
 
 const primitives = jsonData["tokens/primatives"];
+const variablesMode1 = jsonData["variables/Mode 1"];
 
 let cssContent = ":root {\n";
+
 for (const [name, data] of Object.entries(primitives)) {
   cssContent += `  --${name}: ${data.value};\n`;
+}
+
+for (const [name, data] of Object.entries(variablesMode1)) {
+  const reference = data.value.replace(/{|}/g, "");
+  if (primitives[reference]) {
+    cssContent += `  --${name}: var(--${reference});\n`;
+  } else {
+    cssContent += `  --${name}: ${data.value};\n`;
+  }
 }
 
 cssContent += "}";
