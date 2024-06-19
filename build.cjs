9117b503e3d949a29f39c3f26ee1176b7d4bbc5f
Maria <maria123321borovikova@gmail.com>
2024-06-19 17:32:16 +0200

    test github

--- build.cjs
+++ build.cjs
@@ -5,19 +5,11 @@ const jsonFilePath = path.join(__dirname, "tokens", "tokens.json");
 
 const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, "utf8"));
 
-const primitives = jsonData["Variable collection/primatives"];
-const tokens = jsonData["tokens/colors"];
+const primitives = jsonData["tokens/primatives"];
 
 let cssContent = ":root {\n";
 for (const [name, data] of Object.entries(primitives)) {
   cssContent += `  --${name}: ${data.value};\n`;
-}
-
-for (const [name, data] of Object.entries(tokens)) {
-  const ref = data.value.slice(1, -1);
-  if (primitives[ref]) {
-    cssContent += `  --${name}: var(--${ref});\n`;
-  }
 }
 
 cssContent += "}";
