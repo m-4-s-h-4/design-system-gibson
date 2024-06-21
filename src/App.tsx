03d430b4c0adc7b5e9ec06d98860025b92873aee
Maria <maria123321borovikova@gmail.com>
2024-06-21 11:46:35 +0200

    chnages to styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
-import "./App.css";
-import React from "react";
+import Heading from "./components/Heading";
+import "./tokens/css/_variables.css";
 
 function App() {
-  return <p>Click on the Vite and React logos to learn more</p>;
+  return (
+    <div>
+      <Heading as="h1" variant="heading10">
+        This is title
+      </Heading>
+      <Heading as="h2" variant="heading20">
+        This is title
+      </Heading>
+      <Heading as="h3" variant="heading30">
+        This is title
+      </Heading>
+      <Heading as="h4" variant="heading40">
+        This is title
+      </Heading>
+      <Heading as="h5" variant="heading50">
+        This is title
+      </Heading>
+    </div>
+  );
 }
 
 export default App;
