b2c5d339a6593130197418fc03c60a46b4c2239e
Maria <maria123321borovikova@gmail.com>
2024-06-23 18:47:33 +0200

    new icons

--- src/components/icons/Success.tsx
+++ src/components/icons/Success.tsx
@@ -6,17 +6,24 @@ interface SvgSuccessProps extends SVGProps<SVGSVGElement> {
 
 const Success = ({ size = "1em", ...props }: SvgSuccessProps) => (
   <svg
+    xmlns="http://www.w3.org/2000/svg"
     width={size}
     height={size}
     viewBox="0 0 10 8"
     fill="none"
-    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path
-      d="M3.5002 6.09999L1.4002 3.99999L0.700195 4.69999L3.5002 7.49999L9.5002 1.49999L8.8002 0.799988L3.5002 6.09999Z"
-      fill="#42B983"
-    />
+    <g clipPath="url(#success_svg__a)">
+      <path
+        fill="currentColor"
+        d="M3.5002 6.09999L1.4002 3.99999L0.700195 4.69999L3.5002 7.49999L9.5002 1.49999L8.8002 0.799988L3.5002 6.09999Z"
+      />
+    </g>
+    <defs>
+      <clipPath id="success_svg__a">
+        <path fill="#fff" d="M0 0h10v8H0z" />
+      </clipPath>
+    </defs>
   </svg>
 );
 
